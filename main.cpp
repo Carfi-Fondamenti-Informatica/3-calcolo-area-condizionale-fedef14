f8066b9598ca30e87724f171d4a334ef0c849724
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-11-17 16:19:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main() {
+   float a = 0;
+   float b = 0;
+   int opzione = 0;
+   float area = 0;
+   
+   cin >> a >> b >> opzione;
+   
+   switch(opzione){
+      case 0:
+         area = (a*b)/2;
+         break;
+      case 1:
+         area = pow(a,2);
+         break;
+      case 2:
+         area = a*b;
+         break;
+      default: 
+         cout << "opzione non valida" << endl;
+   }
+   cout << area << endl;
    return 0;
 }
